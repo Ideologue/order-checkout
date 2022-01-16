dc6011f07301bf9f96c8ef6f7d0e9f56f65f290b
Ibrahim Farhat <iFarhat@agiletz.com>
2022-01-16 21:05:28 +0200

    fix auto create tables

--- src/main/java/com/almenuz/basket/checkout/Application.java
+++ src/main/java/com/almenuz/basket/checkout/Application.java
@@ -1,10 +1,13 @@
 package com.almenuz.basket.checkout;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-
+@EntityScan("com.almenuz.basket.checkout.model")
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
 
 	public static void main(String[] args) {
